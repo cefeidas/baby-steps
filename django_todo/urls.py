1da23a15bf55e242558b459340a3b62d3da677fb
cefeidas <>
2023-08-08 20:02:45 +0000

    Initialize Django project 'django_todo' and create app 'todo'

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import say_hello
+from todo.views import library_project
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name='hello')
+    path('', library_project, name='library_project')
 ]
