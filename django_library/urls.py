d1acffb95d02922e80bc1ea20477fb7cab5278d2
cefeidas <>
2023-08-10 15:01:56 +0000

    Adapted the 'todo' app and 'django_todo' project to a library system. Created HTML skeleton for the library's home page.

--- django_library/urls.py
+++ django_library/urls.py
@@ -1,4 +1,4 @@
-"""django_todo URL Configuration
+"""django_library URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import library_project
+from library.views import library_project
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- library/views.py
+++ library/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def library_project(request):
-    return render(request, 'todo/todo_list.html')
+    return render(request, 'library/library_home.html')
