1546829f1af2f0dda8de6ed109e81e0ec79d46f7
cefeidas <>
2023-08-20 21:10:13 +0000

    fix: relocate files

--- library/admin.py
+++ library/admin.py
@@ -1,4 +1,46 @@
+import sys
+print("Python Paths:", sys.path)
+
+import csv
 from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
 from .models import Book
-# Register your models here.
-admin.site.register(Book)
+from .forms import CSVImportForm
+
+class BookAdmin(admin.ModelAdmin):
+    change_list_template = 'library/admin/book_changelist.html'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import-csv/', self.import_csv),
+        ]
+        return my_urls + urls
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES["csv_file"]
+            reader = csv.reader(csv_file.read().decode('utf-8').splitlines())
+            for row in reader:
+                book = Book(
+                    title=row[0],
+                    description=row[1],
+                    genre=row[2],
+                    num_pages=int(row[3]),
+                    editorial=row[4],
+                    isbn=row[5],
+                    year_edition=int(row[6]),
+                    date_edition=row[7],
+                    writer=row[8],
+                    image_url=row[9],
+                )
+                book.save()
+            self.message_user(request, "CSV file has been imported")
+            return redirect("..")
+        form = CSVImportForm()
+        payload = {"form": form}
+        return render(request, "admin/csv_form.html", payload)
+
+admin.site.register(Book, BookAdmin)
+
--- library/views.py
+++ library/views.py
@@ -9,19 +9,27 @@ def library_home(request):
     return render(request, 'library/library_home.html')
 
 
+from django.core.paginator import Paginator
+from .models import Book
+
 def catalog(request):
     query = request.GET.get('query', '')
     field = request.GET.get('field', 'title')
-    books = Book.objects.all()
+    books_list = Book.objects.all()
     if query:
         filter_args = {f'{field}__icontains': query}
-        books = books.filter(**filter_args)
+        books_list = books_list.filter(**filter_args)
+
+    paginator = Paginator(books_list, 5) # Show 5 books per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'books': books,
+        'books': page_obj,
         'query': query,
     }
     return render(request, 'library/catalog.html', context)
+
 
 
 
